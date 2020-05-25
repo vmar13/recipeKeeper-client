8125bffc8fa5ab24d48b26cdfe996a3256d09cd1
jnzhang622 <jnzhang622@gmail.com>
2020-05-25 15:32:01 -0400

    fixed dropdown filter

--- assets/src/index.js
+++ assets/src/index.js
@@ -10,104 +10,111 @@ const recipeDiv = document.createElement('div')
 
 document.addEventListener('DOMContentLoaded', () => {
   const fetchRecipes = () => {
-      fetch(recipesUrl)
-        .then(resp => resp.json())
-        .then(populateRecipeCards)
+    fetch(recipesUrl)
+      .then(resp => resp.json())
+      .then(populateRecipeCards)
   }
 
-const populateRecipeCards = recipes => {
-  recipes.forEach(recipe => {
-    
-    //create cards for each recipe
-    recipeDiv.className = "card"
-    cardContainer.appendChild(recipeDiv)
+  const populateRecipeCards = recipes => {
+    recipes.forEach(recipe => {
 
-    const frontCard = document.createElement('div')
-    frontCard.className = "front"
-    recipeDiv.appendChild(frontCard)
+      //create cards for each recipe
+      recipeDiv.className = "card"
+      cardContainer.appendChild(recipeDiv)
 
-    const backCard = document.createElement('div')
-    backCard.className = "back"
-    recipeDiv.appendChild(backCard)
+      const frontCard = document.createElement('div')
+      frontCard.className = "front"
+      recipeDiv.appendChild(frontCard)
 
-    frontCard.innerHTML = `
+      const backCard = document.createElement('div')
+      backCard.className = "back"
+      recipeDiv.appendChild(backCard)
+
+      frontCard.innerHTML = `
       <img src= ${recipe.image_url}>`
-      
-    backCard.innerHTML = `
+
+      backCard.innerHTML = `
       ${recipe.ingredients}
       `
 
 
-    //card anime js
-    // const card = document.querySelector(".card");
-    // let playing = false;
+      //card anime js
+      // const card = document.querySelector(".card");
+      // let playing = false;
 
-    // card.addEventListener('click',function() {
-    //   if(playing)
-    //   return;
-  
-    //     playing = true;
-    //     anime({
-    //       targets: card,
-    //       scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
-    //       rotateY: {value: '+=180', delay: 200},
-    //       easing: 'easeInOutSine',
-    //       duration: 400,
-    //       complete: function(anim){
-    //         playing = false;
-    //       }
-    //     });
-    // });
-    ///end card anime js
+      // card.addEventListener('click',function() {
+      //   if(playing)
+      //   return;
 
-  })  
-}
- 
-//create select option for each diet
-const fetchDiets = () => {
-  fetch(dietsUrl)
-    .then(resp => resp.json())
-    .then(renderDietsDropdown)
-}
+      //     playing = true;
+      //     anime({
+      //       targets: card,
+      //       scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
+      //       rotateY: {value: '+=180', delay: 200},
+      //       easing: 'easeInOutSine',
+      //       duration: 400,
+      //       complete: function(anim){
+      //         playing = false;
+      //       }
+      //     });
+      // });
+      ///end card anime js
 
-const renderDietsDropdown = diets => {
-  
-  diets.forEach(diet => {
-    const newOption = document.createElement('option')
-    newOption.textContent = diet.name
-    select.add(newOption)
-
-    //add event listener for selections in dropdown menu
-    select.addEventListener('change', (e) => {
-      const fetchRecipeDiets = () => {
-        fetch(recipedietsUrl)
-          .then(resp => resp.json())
-          .then(clickedRecipeDiets)
-      }
-      const clickedRecipeDiets = recipeDiets => {
-        if (e.target.value === diet.name) {
-          recipeDiets.forEach(recipeDiet => {
-            console.log(recipeDiet.diet.filter)
-          })
-          
-        //   recipeDiets.forEach(recipeDiet => {
-        //   })
-          //show recipes with target diet
-        // console.log(recipeDiets.e.target.value)
-
-
-        // recipeDiv.innerHTML = `${diet.name}`
-
-
-      }
-      }
-      fetchRecipeDiets()
     })
-  })
-}
+  }
+
+  //create select option for each diet
+  const fetchDiets = () => {
+    fetch(dietsUrl)
+      .then(resp => resp.json())
+      .then(renderDietsDropdown)
+  }
+
+  const renderDietsDropdown = diets => {
+
+    diets.forEach(diet => {
+      const newOption = document.createElement('option')
+      newOption.textContent = diet.name
+      select.add(newOption)
+
+      //add event listener for selections in dropdown menu
+      select.addEventListener('change', (e) => {
+        const fetchRecipeDiets = () => {
+          fetch(recipedietsUrl)
+            .then(resp => resp.json())
+            .then(clickedRecipeDiets)
+        }
+        const clickedRecipeDiets = recipeDiets => {
+          if (e.target.value === diet.name) {
+            const testest = recipeDiets.filter(recipeDiet => recipeDiet.diet.name === e.target.value)
+            console.log(testest)
+            testest.forEach(test => {
+              recipeDiv.innerHTML = 
+                `<img src= ${test.recipe.image_url}>`
+              cardContainer.appendChild(recipeDiv)
+              recipeDiv.innerHTML += `
+              ${test.recipe.ingredients}`
+            })
+            
+
+            //   recipeDiets.forEach(recipeDiet => {
+            //   })
+            //show recipes with target diet
+            // console.log(recipeDiets.e.target.value)
+
+
+            // recipeDiv.innerHTML = `${diet.name}`
+
+
+          }
+        }
+        fetchRecipeDiets()
+      })
+    })
+  }
 
 
 
-fetchRecipes()
-fetchDiets()
+  fetchRecipes()
+  fetchDiets()
 })
