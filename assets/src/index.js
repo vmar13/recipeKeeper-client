6d58504933a007be5a257c22f56827726e239367
toastcorners <wise.melissa19@gmail.com>
2020-05-26 15:50:54 -0400

    anime.js attempts

--- assets/src/index.js
+++ assets/src/index.js
@@ -5,6 +5,8 @@ const recipedietsUrl = 'http://localhost:3000/api/v1/recipe_diets'
 const cardContainer = document.querySelector(".container")
 const select = document.querySelector('#diet-names')
 const recipeDiv = document.createElement('div')
+const frontCard = document.createElement('div')
+const backCard = document.createElement('div')
 // import anime from 'animejs';
 
 
@@ -22,60 +24,54 @@ document.addEventListener('DOMContentLoaded', () => {
       recipeDiv.className = "card"
       cardContainer.appendChild(recipeDiv)
 
-      const frontCard = document.createElement('div')
+      //const frontCard = document.createElement('div')
       frontCard.className = "front"
       recipeDiv.appendChild(frontCard)
 
-      const backCard = document.createElement('div')
+      //const backCard = document.createElement('div')
       backCard.className = "back"
       recipeDiv.appendChild(backCard)
 
       frontCard.innerHTML = `
-      <img src= ${recipe.image_url}>`
+      <img src= ${recipe.image_url} width="500" height="500">`
 
       backCard.innerHTML = `
       ${recipe.ingredients}
       `
-
-
-      //card anime js
-      // const card = document.querySelector(".card");
-      // let playing = false;
-
-      // card.addEventListener('click',function() {
-      //   if(playing)
-      //   return;
-
-      //     playing = true;
-      //     anime({
-      //       targets: card,
-      //       scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
-      //       rotateY: {value: '+=180', delay: 200},
-      //       easing: 'easeInOutSine',
-      //       duration: 400,
-      //       complete: function(anim){
-      //         playing = false;
-      //       }
-      //     });
-      // });
-      ///end card anime js
-
+  //anime.js
+      //const card = document.querySelector(".container");
+      let playing = false;
+  
+      document.addEventListener('click',function() {
+        console.log(event)
+        if(playing)
+        return;
+          playing = true;
+          anime({
+            targets: recipeDiv,
+            scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
+            rotateY: {value: '+=180', delay: 200},
+            easing: 'easeInOutSine',
+            duration: 400,
+            complete: function(anim){
+            playing = false;
+            }
+          });
+      });
+//end anime
     })
   }
-
   //create select option for each diet
   const fetchDiets = () => {
     fetch(dietsUrl)
       .then(resp => resp.json())
       .then(renderDietsDropdown)
   }
-
   const renderDietsDropdown = diets => {
-
     diets.forEach(diet => {
       const newOption = document.createElement('option')
       newOption.textContent = diet.name
-      select.add(newOption)
+      select.appendChild(newOption)
 
       //add event listener for selections in dropdown menu
       select.addEventListener('change', (e) => {
@@ -89,32 +85,23 @@ document.addEventListener('DOMContentLoaded', () => {
             const testest = recipeDiets.filter(recipeDiet => recipeDiet.diet.name === e.target.value)
             console.log(testest)
             testest.forEach(test => {
+             // if (recipeDiv.innerHTML = ""){
               recipeDiv.innerHTML = 
-                `<img src= ${test.recipe.image_url}>`
-              cardContainer.appendChild(recipeDiv)
-              recipeDiv.innerHTML += `
-              ${test.recipe.ingredients}`
-            })
+                `<img src= ${test.recipe.image_url} width="400" height="500">`
+              cardContainer.appendChild(frontCard)
             
-
-            //   recipeDiets.forEach(recipeDiet => {
-            //   })
-            //show recipes with target diet
-            // console.log(recipeDiets.e.target.value)
-
-
-            // recipeDiv.innerHTML = `${diet.name}`
-
-
+              //else {
+            })
           }
+          fetchRecipeDiets()
         }
-        fetchRecipeDiets()
+       
       })
     })
   }
+    
+    fetchRecipes()
+    fetchDiets()
+  })
 
 
-
-  fetchRecipes()
-  fetchDiets()
-})
