2928d5fd8f6d6eaed2fb2535854d0bd67d275923
toastcorners <wise.melissa19@gmail.com>
2020-05-29 00:46:55 -0400

    added instructions

--- assets/src/index.js
+++ assets/src/index.js
@@ -58,6 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
           ${word}
           `
         })
+        ul.innerHTML += `${recipe.instructions}`
       }
 
       ingredientSplitFunc()
